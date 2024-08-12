391391c88926b7be70c777f4f61b9289660881c4
Sajalrastogi89 <Sajalrastogii89@gmail.com>
2024-08-12 20:18:14 +0530

    ai

--- src/main/java/com/ASSR/AiBot/Service/AiService.java
+++
@@ -1,8 +0,0 @@
-//package com.ASSR.AiBot.Service;
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public interface AiService {
-//    String getResponse(String Question, String Mode);
-//}
